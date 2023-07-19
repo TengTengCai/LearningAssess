e4a3dcd361f19b76e88e7ca1e73ac5d38aa27b41
TengTengCai <1021766585@qq.com>
2023-07-20 00:08:08 +0800

    init project

---
+++ apps/tiku/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TikuConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tiku'
---
+++ tasks.py
@@ -0,0 +1,25 @@
+import configparser
+
+import MySQLdb
+from invoke import task
+BASE_CONFIG = configparser.ConfigParser()
+BASE_CONFIG.read('config.ini')
+MYSQL_SETTINGS = BASE_CONFIG['MYSQL_SETTINGS']
+REDIS_SETTINGS = BASE_CONFIG['MYSQL_SETTINGS']
+
+
+@task
+def build(c):
+    print("Building!")
+
+
+@task
+def create_db(c):
+    conn = MySQLdb.connect(
+        host=MYSQL_SETTINGS['host'],
+        port=int(MYSQL_SETTINGS['port']),
+        user=MYSQL_SETTINGS['user'],
+        passwd=MYSQL_SETTINGS['passwd'],
+    )
+    cur = conn.cursor()
+    cur.execute(f"CREATE DATABASE IF NOT EXISTS {MYSQL_SETTINGS['db']};")
