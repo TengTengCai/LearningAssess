ab133ba8461f6742a2f20c9f49b0c1405d961967
TengTengCai <1021766585@qq.com>
2023-07-22 10:49:12 +0800

    修改设置

--- LearningAssess/settings.py
+++ LearningAssess/settings.py
@@ -170,6 +170,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
-CSRF_TRUSTED_ORIGINS = ['http://localhost']
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# CSRF_TRUSTED_ORIGINS = ['http://localhost']
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
