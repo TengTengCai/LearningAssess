6e9dad3a3bb5ed43d3787cc594467d055c928ec0
TengTengCai <1021766585@qq.com>
2023-07-28 22:37:00 +0800

    修改user必填项

--- apps/wechat/models.py
+++ apps/wechat/models.py
@@ -10,11 +10,11 @@ class User(BaseModel):
         NV = "1", _("女")
 
     openid = models.CharField(max_length=128, unique=True, help_text='小程序中的openid')
-    name = models.CharField(verbose_name='姓名', max_length=16, help_text='用户姓名')
-    sex = models.CharField(verbose_name='性别', max_length=2, choices=SexSelect.choices)
-    age = models.IntegerField(verbose_name='年龄', help_text='用户年龄')
-    grade = models.CharField(verbose_name='年级', max_length=32, help_text='用户年龄')
-    phone = models.CharField(verbose_name='手机号', unique=True, max_length=11, help_text='用户手机号')
+    name = models.CharField(verbose_name='姓名', default='', null=True, blank=True, max_length=16, help_text='用户姓名')
+    sex = models.CharField(verbose_name='性别', default=SexSelect.NAN, null=True, blank=True, max_length=2, choices=SexSelect.choices)
+    age = models.IntegerField(verbose_name='年龄', default=18, null=True, blank=True, help_text='用户年龄')
+    grade = models.CharField(verbose_name='年级', default='', null=True, blank=True, max_length=32, help_text='用户年龄')
+    phone = models.CharField(verbose_name='手机号', default='', null=True, blank=True, max_length=11, help_text='用户手机号')
 
     def __str__(self):
         return f"{self.name}_{self.phone}"
