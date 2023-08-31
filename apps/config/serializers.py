00422674250e43df7527ac16960de2cf86cb5c8d
TengTengCai <1021766585@qq.com>
2023-08-31 12:38:28 +0800

    修改字段名称

--- apps/config/serializers.py
+++ apps/config/serializers.py
@@ -5,63 +5,67 @@ from apps.config.models import CourseInfo, Config, Article
 
 
 class ConfigSerializer(serializers.ModelSerializer):
-    # index_image_url = serializers.SerializerMethodField(read_only=True, method_name='get_index_image_url')
-    # xetong_address_url = serializers.SerializerMethodField(read_only=True, method_name='get_xetong_address_url')
+    index_image_url = serializers.SerializerMethodField(read_only=True, method_name='get_index_image_url')
+    xetong_address_url = serializers.SerializerMethodField(read_only=True, method_name='get_xetong_address_url')
 
     # index_image = serializers.SerializerMethodField(read_only=True, method_name='get_index_image')
 
-    # evaluation_image_url = serializers.SerializerMethodField(read_only=True, method_name='get_evaluation_image_url')
-    # shop_address_url = serializers.SerializerMethodField(read_only=True, method_name='get_shop_address_url')
+    evaluation_image_url = serializers.SerializerMethodField(read_only=True, method_name='get_evaluation_image_url')
+    shop_address_url = serializers.SerializerMethodField(read_only=True, method_name='get_shop_address_url')
 
     # evaluation_image = serializers.SerializerMethodField(read_only=True, method_name='get_evaluation_image')
 
     def get_index_image_url(self, config):
-        request = self.context.get('request')
-        index_image = config.index_image
-        if index_image is None:
-            return ''
-        try:
-            if not hasattr(index_image, 'url'):
-                return ''
-        except ValueError:
-            return ''
-        return request.build_absolute_uri(index_image.url)
+        return config.index_image
+        # request = self.context.get('request')
+        # index_image = config.index_image
+        # if index_image is None:
+        #     return ''
+        # try:
+        #     if not hasattr(index_image, 'url'):
+        #         return ''
+        # except ValueError:
+        #     return ''
+        # return request.build_absolute_uri(index_image.url)
 
     def get_evaluation_image_url(self, config):
-        request = self.context.get('request')
-        evaluation_image = config.evaluation_image
-        if evaluation_image is None:
-            return ''
-        try:
-            if not hasattr(evaluation_image, 'url'):
-                return ''
-        except ValueError:
-            return ''
-        return request.build_absolute_uri(evaluation_image.url)
+        return config.evaluation_image
+        # request = self.context.get('request')
+        # evaluation_image = config.evaluation_image
+        # if evaluation_image is None:
+        #     return ''
+        # try:
+        #     if not hasattr(evaluation_image, 'url'):
+        #         return ''
+        # except ValueError:
+        #     return ''
+        # return request.build_absolute_uri(evaluation_image.url)
 
     def get_xetong_address_url(self, config):
-        request = self.context.get('request')
-        xetong_address = config.xetong_address
-        if xetong_address is None:
-            return ''
-        try:
-            if not hasattr(xetong_address, 'url'):
-                return ''
-        except ValueError:
-            return ''
-        return request.build_absolute_uri(xetong_address.url)
+        return config.xetong_address
+        # request = self.context.get('request')
+        # xetong_address = config.xetong_address
+        # if xetong_address is None:
+        #     return ''
+        # try:
+        #     if not hasattr(xetong_address, 'url'):
+        #         return ''
+        # except ValueError:
+        #     return ''
+        # return request.build_absolute_uri(xetong_address.url)
 
     def get_shop_address_url(self, config):
-        request = self.context.get('request')
-        shop_address = config.shop_address
-        if shop_address is None:
-            return ''
-        try:
-            if not hasattr(shop_address, 'url'):
-                return ''
-        except ValueError:
-            return ''
-        return request.build_absolute_uri(shop_address.url)
+        return config.shop_address
+        # request = self.context.get('request')
+        # shop_address = config.shop_address
+        # if shop_address is None:
+        #     return ''
+        # try:
+        #     if not hasattr(shop_address, 'url'):
+        #         return ''
+        # except ValueError:
+        #     return ''
+        # return request.build_absolute_uri(shop_address.url)
 
     # def get_index_image(self, config):
     #     index_image = config.index_image
@@ -77,9 +81,9 @@ class ConfigSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Config
-        fields = '__all__'
-        # fields = ['index_image', 'index_image_url', 'xetong_address', 'xetong_address_url', 'shop_address',
-        #           'shop_address_url', 'evaluation_image', 'evaluation_image_url']
+        # fields = '__all__'
+        fields = ['index_image', 'index_image_url', 'xetong_address', 'xetong_address_url', 'shop_address',
+                  'shop_address_url', 'evaluation_image', 'evaluation_image_url']
 
 
 class CourseInfoSerializer(serializers.ModelSerializer):
